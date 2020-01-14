e4c93118e6b82e0992a81b1d6c66876ebe28aa88
Edgar Montes <emontes@nova.edu>
2020-01-14 16:30:34 -0600

    BasicForm

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = ({titulo}) =>  (  
+    <nav>
+        <div className="nav-wrapper light-blue darken-2">
+            <a herf="#!" className="brand-logo">{titulo}</a>
+        </div>
+        
+    </nav>
+
+);
+
+ 
+export default Header;
